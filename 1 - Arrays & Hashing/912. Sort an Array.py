3eb1e86af2083c460157a2a862e7a56ea45102c6
jitianliang <liangjitian777@gmail.com>
2023-03-16 14:24:34 +1100

    day81

--- 1 - Arrays & Hashing/912. Sort an Array.py
+++ 1 - Arrays & Hashing/912. Sort an Array.py
@@ -33,4 +33,39 @@ class Solution:
         # random.shuffle(nums)
         # quickSort(0, len(nums) - 1, nums)
 
+        # Merge Sort
+        def merge(l, m, r, arr):
+            left, right = arr[l : m + 1], arr[m + 1 : r + 1]
+            i, j, k = 0, 0, l
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    arr[k] = left[i]
+                    i += 1
+                else:
+                    arr[k] = right[j]
+                    j += 1
+                k += 1
+            
+            while i < len(left):
+                arr[k] = left[i]
+                i, k = i + 1, k + 1
+            
+            while j < len(right):
+                arr[k] = right[j]
+                j, k = j + 1, k + 1
+
+
+        def mergeSort(l, r, arr):
+            if l == r:
+                return arr
+
+            m = (r + l) // 2
+            mergeSort(l, m, arr)
+            mergeSort(m + 1, r, arr)
+            merge(l, m, r, arr)
+
+        mergeSort(0, len(nums), nums)
+
+        return nums
+
         # return nums
